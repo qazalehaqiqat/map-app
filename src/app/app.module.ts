06c6f8e357b0588c4bfb442cf726ec25e828fb20
ghazaleh <ghazaleh_haghighat@yahoo.com>
2019-07-14 00:29:48 +0430

    add leaflet map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule} from '@angular/common/http';
 import {UserProvider} from '../providers/userProvider';
-import { Map } from 'mapir-angular';
 import { ImagePicker } from '@ionic-native/image-picker/ngx';
 import { Crop } from '@ionic-native/crop/ngx';
 import { File } from '@ionic-native/file/ngx';
@@ -18,15 +16,15 @@ import { File } from '@ionic-native/file/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,Map],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
     UserProvider,
-    Geolocation,
     Crop,
     ImagePicker,
     File,
+    Geolocation,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -2,70 +2,138 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Platform } from '@ionic/angular';
-import { Map, MapirComponent, MapirService } from 'mapir-angular';
+import { HttpClient } from '@angular/common/http';
+
+// import { Map, MapirComponent, MapirService } from 'mapir-angular';
 import { MapOperator } from 'rxjs/internal/operators/map';
-import { map } from 'leaflet';
-//import {  tileLayer, marker } from 'leaflet';
-var lat;
-var lng;
-var currentlocation = {lat: "", lng: ""};
-var L;
+import { Map, tileLayer, marker, icon, latLng, featureGroup , circle} from 'leaflet';
+import * as L from 'leaflet';
+
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage  {
+  lat: any;
+  lng: any;
+  map: Map;
+  constructor(public http: HttpClient, public router: Router,
+              public plt: Platform  , public geolocation: Geolocation,
+              ) { }
 
-  constructor(public router: Router,
-   private geolocation: Geolocation,
-   private map:Map ) { }
 
-
-
-
-  setting(){
-this.router.navigateByUrl('/setting')
-
-  }
-  schedule(){
-    this.router.navigateByUrl('/schedule')
-  }
-  stop(){
-    this.router.navigateByUrl('/stops')
-  }
-
- 
-currentposition(position){
- this. geolocation.getCurrentPosition().then((position) => {
- lat= position.coords.latitude;
- lng= position.coords.longitude;
-         console.log('My latitude : ', position.coords.latitude);
-        console.log('My longitude: ', position.coords.longitude);
-        currentlocation.lat = lat;
-        currentlocation.lng = lng;
-
-       //const markPoint = marker([lat, lng]);
-       //var marker = marker([lat, lng]).addTo(this.map);
-       
+   // tslint:disable-next-line:use-life-cycle-interface
+   ngAfterViewInit() {
+    this.plt.ready().then(() => {
+      this.http.get('https://oghuxxw1e6.execute-api.us-east-1.amazonaws.com/dev')
+      .subscribe(restaurants => this.initMap(restaurants));
     });
-  
+  }
+
+  initMap(restaurants) {
+    console.log(restaurants);
+    // tslint:disable-next-line:no-shadowed-variable
+    navigator.geolocation.getCurrentPosition(loc => {
+
+      const customMarkerIcon = icon({
+        iconUrl: 'assets/icon/loc-1.png',
+        iconSize: [64, 64],
+        popupAnchor: [0, -20]
+      });
+      // const latlng = new latLng(location.coords.latitude, location.coords.longitude);
+
+      // tslint:disable-next-line:no-shadowed-variable
+      const map = new Map('map');
+
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+      map.locate({
+        setView: true,
+        maxZoom: 10
+      });
+
+      map.on('click', addMarker);
+
+      function addMarker(e) {
+        // Add marker to map at click location; add popup window
+
+        if (current_position !== undefined) {
+          map.removeLayer(current_position);
+        }
+        current_position = L.marker(e.latlng, { icon: customMarkerIcon }).addTo(map);
+    }
+
+
+      // tslint:disable-next-line:variable-name
+      let current_position, current_accuracy;
+
+      function onLocationFound(e) {
+        // if position defined, then remove the existing position marker and accuracy circle from the map
+        if (current_position) {
+            map.removeLayer(current_position);
+            map.removeLayer(current_accuracy);
+        }
+
+        const radius = e.accuracy / 2;
+
+        current_position = L.marker(e.latlng).addTo(map)
+          .bindPopup('You are within ' + radius + ' meters from this point').openPopup();
+
+        current_accuracy = L.circle(e.latlng, radius).addTo(map);
+      }
+
+      // map.on('locationfound', (e) => {
+      //   const markerGroup = featureGroup();
+      //   this.lat = e.latitude;
+      //   this.lng = e.longitude;
+      //   map.setView([this.lat, this.lng], 17);
+      //   const markerr: any = marker([e.latitude, e.longitude], { icon: customMarkerIcon }).on('click', () => {
+      //     alert('Marker clicked');
+      //   });
+      //   circle(e.latlng, e.accuracy).addTo(map);
+      //   markerGroup.addLayer(markerr);
+      //   map.addLayer(markerGroup);
+      //   }).on('locationerror', (err) => {
+      //     alert(err.message);
+      // });
+
+
+      restaurants.forEach((restaurant) => {
+        marker([restaurant.position.lat, restaurant.position.lgn], { icon: customMarkerIcon })
+          .bindPopup(`<b>${restaurant.title}</b>`, { autoClose: false })
+          .on('click', () => this.router.navigateByUrl('/restaurant'))
+          .addTo(map).openPopup();
+      });
+    });
+  }
+
+  setting() {
+      this.router.navigateByUrl('/setting');
+  }
+  schedule() {
+      this.router.navigateByUrl('/schedule');
+  }
+  stop() {
+      this.router.navigateByUrl('/stops');
+  }
+
+
+// currentposition(position) {
+//  // tslint:disable-next-line:no-shadowed-variable
+//  this.geolocation.getCurrentPosition().then((position) => {
+//  this.lat = position.coords.latitude;
+//  this.lng = position.coords.longitude;
+//  console.log('My latitude : ', position.coords.latitude);
+//  console.log('My longitude: ', position.coords.longitude);
+//  current_position.lat = this.lat;
+//  current_position.lng = this.lng;
+
+//     });
+
+// }
+
 
 }
-}
- // currentlocation(){
-  //  this.geolocation.getCurrentPosition().then((resp) => {
-  //      resp.coords.latitude
-  //     resp.coords.longitude
-  //    }).catch((error) => {
-  //      console.log('Error getting location', error);
-  //    });
-     
-  //    let watch = this.geolocation.watchPosition();
-  //    watch.subscribe((data) => {
-  //     // data can be a set of coordinates, or an error (if an error occurred).
-  //     data.coords.latitude
-  //     data.coords.longitude
-  //    });
-    
-//}}
